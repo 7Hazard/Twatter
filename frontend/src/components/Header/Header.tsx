e4ecf7b2de732e8bd4d52ac3db7c94ef47ae7c2c
Hazard <hazard9909@gmail.com>
2021-11-27 08:48:51 +0100

    Search bar possible to search users

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,25 +1,55 @@
+import { useState } from "react";
+import { Async } from "react-async";
 import { Link } from "react-router-dom";
-import { searchUser } from "../../api";
+import { authorizedGet, searchUser } from "../../api";
 import { getCookie, getToken, isAuthenticated } from "../../cookies";
+import { User } from "../../interfaces/User";
 import "./Header.scoped.css";
 
-export default function() {
-  return (
-    <header>
-      <Link to="/"><img className="logo" src="/logo.gif" alt="Twatter Logo" /></Link>
-      
-      {isAuthenticated() ? (<>
+export default function () {
+
+
+    return (
+        <header>
+            <Link to="/"><img className="logo" src="/logo.gif" alt="Twatter Logo" /></Link>
+
+            {isAuthenticated() ? (<>
+                <SearchBar />
+                <nav>
+                    <Link to="/">Feed</Link>
+                    <Link to="/messages">Messages</Link>
+                    <Link to="/signout">Sign Out</Link>
+                </nav>
+            </>) : (
+                <nav />
+            )}
+        </header>
+    );
+}
+
+function SearchBar() {
+    const [focused, setFocused] = useState(false)
+    const [search, setSearch] = useState("")
+
+    return (
         <div className="search">
-            <input type="text" placeholder="Search" id="SearchText"></input>
+            <input type="text" placeholder="Search" onChange={e => {
+                setSearch(e.currentTarget.value)
+            }} onFocus={e => setFocused(true)} onBlur={e => setTimeout(()=>setFocused(false), 100)} />
+
+            {search.length > 0 && focused && (
+                <div className="search-results">
+                    <Async promise={authorizedGet(`search/users/${search}`)} >
+                        <Async.Pending></Async.Pending>
+                        <Async.Rejected>{(e: Response) => `No results: ${e.status}`}</Async.Rejected>
+                        <Async.Fulfilled<User[]>>
+                            {users => users.map(u => (
+                                <Link to={"/user/" + u.username}>{u.username}</Link>
+                            ))}
+                        </Async.Fulfilled>
+                    </Async>
+                </div>
+            )}
         </div>
-        <nav>
-          <Link to="/">Feed</Link>
-          <Link to="/messages">Messages</Link>
-          <Link to="/signout">Sign Out</Link>
-        </nav>
-      </>) : (
-        <nav />
-      )}
-    </header>
-  );
+    )
 }
